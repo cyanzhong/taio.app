084a24de164b0694c9eb51de5634fdabc1556e59
Ying Zhong <0x00eeee@gmail.com>
2020-10-25 10:52:14 +0800

    🎉 Refactor

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const menuBtn = document.querySelector('.menu_btn');
+const menuNav = document.querySelector('.nav_compact ul');
+const menuItems = document.querySelectorAll('.nav_compact li');
+const bodyElement = document.querySelector('body');
+let isMenuOpened = false;
+
+function closeMenu() {
+  menuBtn.classList.remove('open');
+  menuNav.classList.remove('open');
+  bodyElement.classList.remove('open');
+  isMenuOpened = false;
+}
+
+function openMenu() {
+  menuBtn.classList.add('open');
+  menuNav.classList.add('open');
+  bodyElement.classList.add('open');
+  isMenuOpened = true;
+}
+
+menuBtn.addEventListener('click', () => {
+  if (isMenuOpened) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+menuItems.forEach(item => {
+  item.addEventListener('click', closeMenu);
+});
+
+window.addEventListener('resize', () => {
+  if (!isMenuOpened) {
+    return;
+  }
+
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if (width < 768) {
+    return;
+  }
+
+  closeMenu();
+});
